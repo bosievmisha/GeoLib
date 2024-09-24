0553610c798589ce4639ab112943ee19a9fe29a0
bosievmisha <misha.bosiev@mail.ru>
2024-09-24 12:20:12 +0300

    Edit

---
+++ GeoTest/UnitTests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework.Legacy;
+using GeoLib;
+namespace GeoTest
+{
+    [TestFixture]
+    public class GeometryTests
+    {
+        [Test]
+        public void TestCircleArea()
+        {
+            var circle = new Circle(5);
+            ClassicAssert.AreEqual(Math.PI * 25, circle.calculateArea());
+        }
+
+        [Test]
+        public void TestTriangleArea()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            ClassicAssert.AreEqual(6, triangle.calculateArea());
+        }
+
+        [Test]
+        public void TestRightTriangleCheck()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            ClassicAssert.IsTrue(triangle.isRightTriangle());
+
+            var nonRightTriangle = new Triangle(3, 4, 6);
+            ClassicAssert.IsFalse(nonRightTriangle.isRightTriangle());
+        }
+    }
+}
