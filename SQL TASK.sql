ada3185c7832f99bbbc19e89d92ac9fcac93b619
bosievmisha <167859213+bosievmisha@users.noreply.github.com>
2024-10-04 16:07:11 +0300

    Delete SQL TASK.sql

--- SQL TASK.sql
+++
@@ -1,5 +0,0 @@
-SELECT prod.name [Имя продукта], cat.name [Имя категории]
-FROM Products AS prod
-LEFT JOIN ProductsCategory AS prodcat ON prod.id = prodcat.products_id
-INNER JOIN Category AS cat ON cat.id = prodcat.category_id
-ORDER BY prod.name
